43cdade4f50ffba287353e17c24fc2d81f7f208b
Igor <eliseev88@gmail.com>
2021-06-30 03:35:48 +0300

    Подключен и настроен модуль регистрации и авторизации пользователей Laravel UI

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
 Route::get('/account', function () {
     return view('account.index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
